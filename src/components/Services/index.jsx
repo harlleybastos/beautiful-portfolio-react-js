13043748af79bd145155b18266e72e74d31b646f
Harlley <harlleybastos@hotmail.com>
2022-03-28 19:58:48 -0300

    feat: services section

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,7 +1,164 @@
 import React from "react";
+import {
+  FirstText,
+  SecondText,
+  ServiceContainer,
+  ServiceContainerHead,
+  ServiceContainerHeadTitle,
+  ServiceList,
+  ServiceListItem,
+  ServiceListItemParagraph,
+  ServicesContainer,
+  ServicesWrapper,
+} from "./styles";
+
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <ServicesWrapper>
+      <FirstText>What I Offer</FirstText>
+      <SecondText>Services</SecondText>
+
+      <ServicesContainer>
+        <ServiceContainer>
+          <ServiceContainerHead>
+            <ServiceContainerHeadTitle>UI/UX Design</ServiceContainerHeadTitle>
+          </ServiceContainerHead>
+          <ServiceList>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+          </ServiceList>
+        </ServiceContainer>
+
+        <ServiceContainer>
+          <ServiceContainerHead>
+            <ServiceContainerHeadTitle>
+              Web Development
+            </ServiceContainerHeadTitle>
+          </ServiceContainerHead>
+          <ServiceList>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+          </ServiceList>
+        </ServiceContainer>
+
+        <ServiceContainer>
+          <ServiceContainerHead>
+            <ServiceContainerHeadTitle>
+              Content Creation
+            </ServiceContainerHeadTitle>
+          </ServiceContainerHead>
+          <ServiceList>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+            <ServiceListItem>
+              <BiCheck className="service__list-icon" />
+              <ServiceListItemParagraph>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </ServiceListItemParagraph>
+            </ServiceListItem>
+          </ServiceList>
+        </ServiceContainer>
+      </ServicesContainer>
+    </ServicesWrapper>
+  );
 };
 
 export default Services;
