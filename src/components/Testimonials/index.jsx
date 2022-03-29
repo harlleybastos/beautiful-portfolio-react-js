81054acb9e622c4658b365f68fc3246e92c3e766
Harlley <harlleybastos@hotmail.com>
2022-03-29 18:25:05 -0300

    feat: testimonials component

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,7 +1,84 @@
 import React from "react";
+import {
+  FirstText,
+  SecondText,
+  TestimonialContainerClientAvatar,
+  TestimonialContainerClientAvatarClientsReview,
+  TestimonialContainerClientAvatarImage,
+  TestimonialContainerClientAvatarName,
+  TestimonialsContainer,
+  TestimonialsWrapper,
+} from "./styles";
+
+import Avatar1 from "../../assets/img/avatar1.jpg";
+import Avatar2 from "../../assets/img/avatar2.jpg";
+import Avatar3 from "../../assets/img/avatar3.jpg";
+import Avatar4 from "../../assets/img/avatar4.jpg";
+
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: Avatar1,
+    name: "Client 01",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi quisquam ullam! Ducimus fugit ab quaerat corporis deleniti quam repudiandae earum sed. Molestiae libero magni maxime ducimus corporis placeat enim.",
+  },
+  {
+    avatar: Avatar2,
+    name: "Client 02",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi quisquam ullam! Ducimus fugit ab quaerat corporis deleniti quam repudiandae earum sed. Molestiae libero magni maxime ducimus corporis placeat enim.",
+  },
+  {
+    avatar: Avatar3,
+    name: "Client 03",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi quisquam ullam! Ducimus fugit ab quaerat corporis deleniti quam repudiandae earum sed. Molestiae libero magni maxime ducimus corporis placeat enim.",
+  },
+  {
+    avatar: Avatar4,
+    name: "Client 04",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi quisquam ullam! Ducimus fugit ab quaerat corporis deleniti quam repudiandae earum sed. Molestiae libero magni maxime ducimus corporis placeat enim.",
+  },
+];
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <TestimonialsWrapper>
+      <FirstText>Review from clients</FirstText>
+      <SecondText>Testimonials</SecondText>
+
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        className="container testimonials__container"
+      >
+        {data.map(({ avatar, name, review }, index) => (
+          <SwiperSlide key={index} className="testimonial">
+            <TestimonialContainerClientAvatar>
+              <TestimonialContainerClientAvatarImage src={avatar} alt={name} />
+            </TestimonialContainerClientAvatar>
+            <TestimonialContainerClientAvatarName>
+              {name}
+            </TestimonialContainerClientAvatarName>
+            <TestimonialContainerClientAvatarClientsReview>
+              {review}
+            </TestimonialContainerClientAvatarClientsReview>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </TestimonialsWrapper>
+  );
 };
 
 export default Testimonials;
--- src/components/Testimonials/styles.js
+++ src/components/Testimonials/styles.js
@@ -1,1 +1,68 @@
 import styled from "styled-components";
+
+export const TestimonialsWrapper = styled.section.attrs({
+  id: "testimonials",
+})`
+  .testimonial {
+    background: var(--color-bg-variant);
+    text-align: center;
+    padding: 2rem;
+    border-radius: 2rem;
+    user-select: none;
+  }
+  .testimonials__container {
+    width: 40%;
+    padding-bottom: 4rem;
+
+    @media screen and (max-width: 1024px) {
+      width: 60%;
+    }
+    @media screen and (max-width: 600px) {
+      width: var(--container-width-sm);
+    }
+  }
+
+  .swiper-pagination-clickable .swiper-pagination-bullet {
+    background: var(--color-primary);
+  }
+`;
+
+export const FirstText = styled.h5``;
+
+export const SecondText = styled.h2``;
+
+export const TestimonialsContainer = styled.div.attrs({
+  className: "container",
+})``;
+
+export const TestimonialContainer = styled.article`
+  background: var(--color-bg-variant);
+  text-align: center;
+  padding: 2rem;
+  border-radius: 2rem;
+  user-select: none;
+`;
+
+export const TestimonialContainerClientAvatar = styled.div`
+  width: 4rem;
+  aspect-ratio: 1/1;
+  overflow: hidden;
+  border-radius: 50%;
+  margin: 0 auto 1rem;
+  border: 0.4rem solid var(--color-primary-variant);
+`;
+
+export const TestimonialContainerClientAvatarImage = styled.img``;
+
+export const TestimonialContainerClientAvatarName = styled.h5``;
+
+export const TestimonialContainerClientAvatarClientsReview = styled.small`
+  color: var(--color-light);
+  font-weight: 300;
+  display: block;
+  width: 80%;
+  margin: 0.8rem auto 0;
+  @media screen and (max-width: 600px) {
+    width: var(--container-width-sm);
+  }
+`;
